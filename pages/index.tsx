f8763bcaab3c0e4bfe50a0c06ec281c17e64a25b
Yusuf Emirhan <syusufemirhan@gmail.com>
2021-09-09 21:31:47 +0300

    önemli

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import styles from "../styles/Home.module.scss";
 import Link from "next/link";
+import Head from 'next/head'
+
 const { BLOG_URL, CONTENT_API_KEY } = process.env;
 
 type Post = {
@@ -20,21 +22,13 @@ export const getStaticProps = async ({ params }) => {
 
 const Home: React.FC<{ posts: Post[] }> = (props) => {
   const { posts } = props;
-  return (
-    <div className={styles.container}>
-      <h1>Merhaba!</h1>
-      <ul>
-        {posts.map((post, index) => {
-          return (
-            <li key={post.slug}>
-              <Link href="/post/[slug]" as={`/post/${post.slug}`}>
-                <a>{post.title}</a>
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+  return (<>
+    <Head>
+      <title>Önemli</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <p>İlk kullanımı doğum tarihlerini belirtmek içindir. Orijinal şekilde altı kollu idi ve her kol bir damla şeklindeydi. Daktilolarda bu şekilde yer almıştır. Bununla beraber bazı daktiloların her kolu ayrı bir şekilde basmakta güçlük çekmiştir. Ayrıca Arap-İsrail ihtilafı nedeniyle birçok Arap, altı kollu sembolü barındıran daktiloları almayı tercih etmemiştir. Bunun sebebi, altı kollu sembolü İsrail bayrağında bulunan Davud Yıldızı'na benzetmeleriydi. Bu nedenle Arap yıldızı olarak anılan beş köşeli bir yıldız ayrı bir sembol olarak birçok sistemde kullanılmıştır. Bununla birlikte bazı fontlarda (yazı biçimi) Arap yıldızı sekiz köşelidir. İki sembolü aşağıda görebilirsiniz:
+      AsteriskArap yıldızı*٭</p></>
   );
 };
 export default Home;
